a7c7cb851e4dac637b0523d29ce508a631fbc7af
sdy876 <sdy876@qq.com>
2021-11-25 14:44:44 +0800

    main

--- src/main.cpp
+++ src/main.cpp
@@ -33,16 +33,16 @@ void Tripleflash()
       pixels.setPixelColor(j, pixels.Color(150, 0, 0));
     }
     pixels.show();
-    delay(100);
+    delay(50);
     for (int j = 0; j < 3; j++)
     {
       pixels.setPixelColor(j, pixels.Color(0, 0, 0));
     }
     pixels.show();
-    delay(100);
+    delay(DELAYVAL);
   }
   pixels.clear();
-  delay(DELAYFOO);
+  delay(50);
   for (int i = 0; i < 3; i++)
   {
     for (int j = 6; j < 9; j++)
@@ -50,13 +50,13 @@ void Tripleflash()
       pixels.setPixelColor(j, pixels.Color(0, 0, 150));
     }
     pixels.show();
-    delay(100);
+    delay(50);
     for (int j = 6; j < 9; j++)
     {
       pixels.setPixelColor(j, pixels.Color(0, 0, 0));
     }
     pixels.show();
-    delay(100);
+    delay(50);
   }
   pixels.clear();
   delay(DELAYFOO);
